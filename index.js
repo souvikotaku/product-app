eb38d60277e45a88fa3ed28234fa8d593f2575c4
souvikotaku <manson1989@>
2023-12-12 20:26:48 +0530

    changes

---
+++ index.js
@@ -0,0 +1,15 @@
+import { registerRootComponent } from "expo";
+import { Provider } from "react-redux";
+
+import App from "./App";
+
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(ReduxApp);
